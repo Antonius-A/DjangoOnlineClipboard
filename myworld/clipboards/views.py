20acbc57565f3d3fce35a5107c469292f67d3d68
Anthony Aboussafy <91700433+Antonius-A@users.noreply.github.com>
2022-12-04 03:28:21 -0500

    random alpha numeric display bug fixed

--- myworld/clipboards/views.py
+++ myworld/clipboards/views.py
@@ -8,7 +8,7 @@ import random, string
 
 def random_generate_alpha_numberic(length: int = 16):
     ui_string = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-    # print(ui_string)
+    print(ui_string)
     return ui_string
 
 
